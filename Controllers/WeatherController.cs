64a813ec87f7aaa143690d1380c9486f272e83a0
Lucian Ciuraru <ciuraru.lucian8@yahoo.com>
2026-06-12 10:58:24 +0100

    Initial Weather API setup

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAPI.Controllers
+{
+    [Route("api/weather")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+
+        // Insert here the dependency injection after you get the Api Key
+        [HttpGet("{city}")]
+        public IActionResult Get(string city)
+        {
+            try
+            {
+                var weather = new { City = city, Temperature = 22, Condition = "Sunny", Humidity = 60 };
+
+                return Ok(weather);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode (500, new {error = ex.Message});
+            }
+        }
+    }
+}
